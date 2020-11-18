6363ebe9dcc3b6e1641cc4598e91af817c037ee3
gee92 <boardleygeneara@gmail.com>
2020-11-17 20:18:01 -0500

    #3

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let num1 = Math.floor(Math.random() * 11);
-let num2 = Math.floor(Math.random() * 11);
+//let num1 = Math.floor(Math.random() * 11);
+//let num2 = Math.floor(Math.random() * 11);
 
-let answer = Number(num1 + num2);
+//let answer = Number(num1 + num2);
 
-let question = prompt("What is " + num1 + " plus " + num2 + "?");
+//let question = prompt("What is " + num1 + " plus " + num2 + "?");
 
-if(Number(question) === Number(answer)) {
-    alert("You're right!");
-} else {
-    alert("You're wrong!");
-}
+//if(Number(question) === Number(answer)) {
+  //  alert("You're right!");
+//} else {
+  //  alert("You're wrong!");
+//}
+
+let num1 = prompt ("How Much do you weigh?");
+let num2 = prompt ("How tall are you? in inches");
+let num3 = (num2 / Math.pow(num1,2)
+
+console.log()
