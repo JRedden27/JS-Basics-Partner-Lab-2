eb9b811ac9b437ae71e1604ff8638b4d855e7846
unknown <DBenson05@yahoo.com>
2020-11-17 21:10:36 -0500

    part 2

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let num1 = Math.floor(Math.random() * 11);
-let num2 = Math.floor(Math.random() * 11);
+//let num1 = Math.floor(Math.random() * 11);
+//let num2 = Math.floor(Math.random() * 11);
 
-let answer = Number(num1 + num2);
+//let answer = Number(num1 + num2);
 
-let question = prompt("What is " + num1 + " plus " + num2 + "?");
+//let question = prompt("What is " + num1 + " plus " + num2 + "?");
 
-if(Number(question) === Number(answer)) {
-    alert("You're right!");
-} else {
-    alert("You're wrong!");
-}
+//if(Number(question) === Number(answer)) {
+  //  alert("You're right!");
+//} else {
+  //  alert("You're wrong!");
+//
+
+let num1 = prompt ("What Is The Radius");
+const pi =  3.14;
+
+let answer = (pi * Math.pow(num1, 2));
